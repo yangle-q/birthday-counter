dccbf980067aaff09e55a86a5c6d3a9b061144f1
Yangle <Yangle.Q@outlook.com>
2025-09-14 21:53:04 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Aug, 2010',
+  name: 'Yangle'
 };
 
 function hideEverything() {
